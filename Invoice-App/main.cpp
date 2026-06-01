dcc939c7fdb5b31741909e42ca294fc8ce6be662
Sriram Jeganathan <sriram.jeganathan@outlook.com>
2026-06-01 11:10:43 +0530

    file updated

--- Invoice-App/main.cpp
+++ Invoice-App/main.cpp
@@ -1,11 +1,26 @@
 #include <QApplication>
-#include "mainwindow.h"
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QDebug>
 
 int main ( int argc, char ** argv ) {
   QApplication theApp( argc, argv );
 
-  MainWindow window;
-  window.show();
+  QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+
+  db.setHostName("localhost");
+
+  db.setDatabaseName("invoice_system");
+
+  db.setUserName("sriram");
+
+  db.setPassword("root");
+
+  if ( db.open() ) {
+    qDebug() << "Database Connected";
+  } else {
+    qDebug() << db.lastError().text();
+  }
 
   return theApp.exec();
 }
